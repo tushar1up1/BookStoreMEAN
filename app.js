43c29ba2618ad82d6becc59e08e19e8d78339fe3
Tushar Upadhyay <tusharup@gmail.com>
2020-07-03 13:45:54 -0400

    created build folder, now all the compiled js will be in build directory. server takes reference of js files from build directory

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname,'book-public', 'src')));
+//app.use(express.static(path.join(__dirname,'book-public', 'src')));
+app.use(express.static(path.join(__dirname, 'book-public', 'build')));
 
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
